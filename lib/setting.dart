7eeca53d498069830e53b61728c224fa3bb9f7d8
dleh1541 <dleh1541@naver.com>
2023-09-11 17:54:58 +0900

    SharedPreference 이용한 알림음 선택 구현

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingScreen extends StatefulWidget {
   @override
@@ -8,12 +9,41 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
-  String selectedOption = 'beep'; // 초기 선택값 설정
+  late String selectedOption = 'none'; // 초기 선택값 설정
   AudioPlayer player = AudioPlayer(); // 오디오 객체 생성
+  late SharedPreferences prefs;
+  late List<String> splitedStr;
 
   @override
   void initState() {
     super.initState();
+    initPrefs();
+  }
+
+  Future<void> initPrefs() async {
+    print("initPrefs() 호출");
+    prefs = await SharedPreferences.getInstance();
+
+    if (prefs.getBool('isUserFile') == false) {
+      setState(() {
+        selectedOption = prefs.getString('selectedSound')!;
+      });
+    } else {
+      setState(() {
+        // selectedOption = prefs.getString('filePath')!;
+        var filePath = prefs.getString('filePath');
+        splitedStr = filePath!.split('/');
+        selectedOption = splitedStr.last;
+      });
+    }
+
+    // 모든 키를 가져오고 각 키에 대한 값을 출력
+    final allKeys = prefs.getKeys();
+    for (final key in allKeys) {
+      final value = prefs.get(key);
+      print('Key: $key, Value: $value');
+    }
+    // 나머지 초기화 작업 수행
   }
 
   // Future audioPlayer() async {
@@ -29,6 +59,9 @@ class _SettingScreenState extends State<SettingScreen> {
     await player.setVolume(1);
     await player.setSpeed(1);
     await player.setAsset(audioAsset); // 선택한 옵션에 따라 다른 mp3 파일 설정
+
+    // await initPrefs();
+
     player.play();
   }
 
@@ -40,13 +73,18 @@ class _SettingScreenState extends State<SettingScreen> {
     if (result != null) {
       PlatformFile file = result.files.first;
       String filePath = file.path!; // 선택한 파일의 경로
-      List<String> splitedStr = filePath.split('/');
+      // List<String> splitedStr = filePath.split('/');
+      splitedStr = filePath.split('/');
 
       print("splitedStr: ${splitedStr}");
       print("splitedStr[last]: ${splitedStr.last}");
 
+      await prefs.setBool('isUserFile', true);
+      await prefs.setString('filePath', filePath);
+      await prefs.setString('selectedSound', 'none');
       await player.stop(); // 기존 오디오 정지
       await player.setFilePath(filePath); // 새로운 파일 설정
+
       setState(() {
         selectedOption = splitedStr.last;
       });
@@ -91,6 +129,9 @@ class _SettingScreenState extends State<SettingScreen> {
                         setState(() {
                           selectedOption = value!;
                         });
+                        prefs.setBool('isUserFile', false);
+                        prefs.setString('selectedSound', 'beep');
+                        prefs.setString('filePath', 'none');
                         audioPlayer('assets/audio/beep.mp3');
                       },
                     ),
@@ -101,8 +142,11 @@ class _SettingScreenState extends State<SettingScreen> {
                       onChanged: (value) {
                         setState(() {
                           selectedOption = value!;
-                          audioPlayer('assets/audio/beep2.mp3');
                         });
+                        prefs.setBool('isUserFile', false);
+                        prefs.setString('selectedSound', 'beep2');
+                        prefs.setString('filePath', 'none');
+                        audioPlayer('assets/audio/beep2.mp3');
                       },
                     ),
                     RadioListTile(
@@ -112,8 +156,11 @@ class _SettingScreenState extends State<SettingScreen> {
                       onChanged: (value) {
                         setState(() {
                           selectedOption = value!;
-                          audioPlayer('assets/audio/beep3.mp3');
                         });
+                        prefs.setBool('isUserFile', false);
+                        prefs.setString('selectedSound', 'beep3');
+                        prefs.setString('filePath', 'none');
+                        audioPlayer('assets/audio/beep3.mp3');
                       },
                     ),
                     RadioListTile(
@@ -123,8 +170,11 @@ class _SettingScreenState extends State<SettingScreen> {
                       onChanged: (value) {
                         setState(() {
                           selectedOption = value!;
-                          audioPlayer('assets/audio/chicken.mp3');
                         });
+                        prefs.setBool('isUserFile', false);
+                        prefs.setString('selectedSound', 'chicken');
+                        prefs.setString('filePath', 'none');
+                        audioPlayer('assets/audio/chicken.mp3');
                       },
                     ),
                     RadioListTile(
@@ -134,8 +184,11 @@ class _SettingScreenState extends State<SettingScreen> {
                       onChanged: (value) {
                         setState(() {
                           selectedOption = value!;
-                          audioPlayer('assets/audio/playtime.mp3');
                         });
+                        prefs.setBool('isUserFile', false);
+                        prefs.setString('selectedSound', 'playtime');
+                        prefs.setString('filePath', 'none');
+                        audioPlayer('assets/audio/playtime.mp3');
                       },
                     ),
                   ],
